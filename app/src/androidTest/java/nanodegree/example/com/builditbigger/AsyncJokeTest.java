887ad683e1a122c2138ec1dfb3f36ea3db69b738
parth12 <parthpanchal12196@gmail.com>
2016-09-26 18:34:33 +0530

    implemented review changes

--- app/src/androidTest/java/nanodegree/example/com/builditbigger/AsyncJokeTest.java
+++ app/src/androidTest/java/nanodegree/example/com/builditbigger/AsyncJokeTest.java
@@ -27,37 +27,11 @@ public class AsyncJokeTest extends AndroidTestCase {
         }
     }
 
-//    public void testJokeAsyncTaskResponse() throws Throwable {
-//
-//        final TestJokeAsyncTask testJokeAsyncTask = new TestJokeAsyncTask(null);
-//        testJokeAsyncTask.execute();
-//        signal.await(); //Wait till AsyncTask finishes
-//    }
-//
-//    public void testResponse(){
-//        final CountDownLatch cd=new CountDownLatch(1);
-//        Long start=new Date().getTime();
-//
-//        new AsyncTaskJoke().execute(new iAsynTaskJokeCallback() {
-//            @Override
-//            public void callbackAsync(String joke) {
-//                assertNotNull(joke);
-//                cd.countDown();
-//            }
-//        });
-//
-//        try{
-//            if(!cd.await(30, TimeUnit.SECONDS))
-//            {
-//                Long end = (new Date().getTime() - start) / 1000;
-//                throw new AssertionError("Service not responding after " + end + " seconds");
-//            }
-//
-//        }
-//        catch (InterruptedException e)
-//        {
-//            e.printStackTrace();
-//        }
-//    }
+    public void testJokeAsyncTaskResponse() throws Throwable {
+
+        final TestJokeAsyncTask testJokeAsyncTask = new TestJokeAsyncTask(null);
+        testJokeAsyncTask.execute();
+        signal.await(); //Wait till AsyncTask finishes
+    }
 
 }
